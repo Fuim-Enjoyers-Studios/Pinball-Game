b0cb76fc62cc692e58ec221b3bb9523c7a911706
franciscofp4 <franciscofp4@TTC-104-28>
2022-11-07 13:55:15 +0100

    Add project files.

---
+++ ModuleSceneMenu.cpp
@@ -0,0 +1,75 @@
+#include "ModuleSceneMenu.h"
+
+#include "Application.h"
+
+SceneMenu::~SceneMenu()
+{
+
+}
+
+// Load assets
+bool SceneMenu::Start()
+{
+	/*LOG("Loading Scene Menu");
+
+	pugi::xml_node configNode = app->LoadConfigFileToVar();
+	pugi::xml_node config = configNode.child(name.GetString());
+
+	menu.PushBack({ 0,0,512,384 });
+	menu.PushBack({ 0,384,512,384 });
+	menu.PushBack({ 512,384,512,384 });
+	menu.PushBack({ 512,0,512,384 });
+	menu.loop = false;
+	menu.speed = 0.1f;
+
+	menuTexture = app->tex->Load(config.child("menu").attribute("texturepath").as_string());
+
+	app->render->camera.x = 0;
+	app->render->camera.y = 0;*/
+
+	return true;
+}
+
+bool SceneMenu::Update(float dt)
+{
+	/*if (appStart) {
+		if (iconCounter <= 120) {
+			appStart = false;
+		}
+		iconCounter--;
+	}
+	else if (iconCounter > 0) {
+		menu.Update();
+		iconCounter--;
+	}
+	else {
+		if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+			app->fade->FadeToBlack(this, (Module*)app->sceneLevel1, 30);
+		}
+	}*/
+
+	return true;
+}
+
+//bool SceneMenu::PostUpdate()
+//{
+//	bool ret = true;
+//
+//	// Draw everything --------------------------------------
+//	SDL_Rect rect = menu.GetCurrentFrame();
+//	app->render->DrawTexture(menuTexture, 0, 0, &rect);
+//
+//	if (app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+//		ret = false;
+//
+//	return ret;
+//}
+
+bool SceneMenu::CleanUp()
+{
+	LOG("Deleting background assets");
+
+	//app->tex->Unload(menuTexture);
+
+	return true;
+}
---
+++ ModuleSceneMenu.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef __SCENE_MENU_H__
+#define __SCENE_MENU_H__
+
+#include "Module.h"
+#include "Animation.h"
+
+struct SDL_Texture;
+
+class SceneMenu : public Module
+{
+public:
+	//Constructor
+	SceneMenu();
+
+	//Destructor
+	~SceneMenu();
+
+	// Called when the module is activated
+	bool Start();
+
+	// Called at the middle of the application loop
+	// Updates the scene's background animations
+	bool Update(float dt);
+
+	// Called at the end of the application loop.
+	// Performs the render call of all the parts of the scene's background
+	//bool PostUpdate();
+
+	bool CleanUp();
+
+public:
+	// The scene sprite sheet loaded into an SDL_Texture
+	SDL_Texture* menuTexture = nullptr;
+	Animation menu;
+	Animation icon;
+
+	int iconCounter = 240;
+	bool appStart = true;
+};
+
+#endif

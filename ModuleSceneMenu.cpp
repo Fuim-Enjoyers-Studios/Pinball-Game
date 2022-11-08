ca535138b7be71111ce7e83ca20c349bef5ec5f4
Luis González <99800056+punto16@users.noreply.github.com>
2022-11-08 20:12:21 +0100

    NOW MENU AVIABLE WITH MOUSE CLICKS!!!

--- ModuleSceneMenu.cpp
+++ ModuleSceneMenu.cpp
@@ -48,6 +48,27 @@ bool SceneMenu::Start()
 
 update_status SceneMenu::Update()
 {
+	//click start with mouse
+	if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN &&
+		App->input->GetMouseX() > 300 &&
+		App->input->GetMouseX() < 500 &&
+		App->input->GetMouseY() > 563 &&
+		App->input->GetMouseY() < 611)
+	{
+		App->fade->FadeToBlack(this, (Module*)App->scene_intro, 60);
+	}
+	//click exit with mouse
+	if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN &&
+		App->input->GetMouseX() > 330 &&
+		App->input->GetMouseX() < 470 &&
+		App->input->GetMouseY() > 611 &&
+		App->input->GetMouseY() < 662)
+	{
+		return update_status::UPDATE_STOP;
+	}
+
+
+
 	if (App->input->GetKey(SDL_SCANCODE_RETURN) == KEY_DOWN) {
 
 		if (playOrExit == true) {
@@ -79,7 +100,24 @@ update_status SceneMenu::Update()
 		App->renderer->Blit(menuTexture, 0, 0);
 	}
 
-	if (playOrExit == true && transitionanim == false) {
+
+	if (App->input->GetMouseX() > 300 &&
+		App->input->GetMouseX() < 500 &&
+		App->input->GetMouseY() > 563 &&
+		App->input->GetMouseY() < 611)
+	{
+		App->renderer->Blit(iconTexture, 152, 560);
+		playOrExit = true;
+	}
+	else if (App->input->GetMouseX() > 330 &&
+		App->input->GetMouseX() < 470 &&
+		App->input->GetMouseY() > 611 &&
+		App->input->GetMouseY() < 662)
+	{
+		App->renderer->Blit(iconTexture, 152, 615);
+		playOrExit = false;
+	}
+	else if (playOrExit == true && transitionanim == false) {
 		App->renderer->Blit(iconTexture, 152, 560);
 	}
 	else if(transitionanim == false){
